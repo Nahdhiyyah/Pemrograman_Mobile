f7e2b92630518857f33f65e86a7ac13ee21c0c10
FlutLab.io <>
2022-03-31 10:12:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BelajarListView(),
+      home: cobaListView(),
     );
   }
 }
 
-class BelajarListView extends StatelessWidget {
-  final List bulan = [
-    "Januari",
-    "Fabruari",
-    "Maret",
-    "April",
-    "Mei",
-    "Juni",
-    "Juli",
-    "Agustus",
-    "September",
-    "Oktober",
-    "November",
-    "Desember"
+class cobaListView extends StatelessWidget {
+  final List gambar = [
+    "assets/images/1.jpg",
+    "assets/images/2.jpg",
+    "assets/images/3.jpg",
+    "assets/images/4.jpg",
+    "assets/images/5.jpg",
+    "assets/images/6.jpg",
+    "assets/images/7.jpg",
+    "assets/images/8.jpg",
+    "assets/images/9.jpg",
+    "assets/images/10.jpg",
+    "assets/images/11.jpg",
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("belajar layout"),
+        title: Text("My Galery", style: TextStyle(color: Colors.black12, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.amber,
       ),
       body: ListView.builder(
+        itemCount: gambar.length,
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
-                title: Text(bulan[index], style: TextStyle(fontSize: 30)),
-                subtitle: Text('ini subtitle dari ' + bulan[index]),
+                title: Text(gambar[index], style: TextStyle(fontSize: 30)),
+                subtitle: Text('ini subtitle dari ' + gambar[index]),
                 leading: CircleAvatar(
-                  child: Text(bulan[index][0], // ambil karakter pertama text
+                  child: Text(gambar[index][0], // ambil karakter pertama text
                       style: TextStyle(fontSize: 20)),
                 )),
           );
         },
-        itemCount: bulan.length,
+        //itemCount: gambar.length,
       ),
     );
   }
