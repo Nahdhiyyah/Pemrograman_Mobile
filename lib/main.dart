2826759a0b4b55c766e95abb9c39fd6361f2b54a
FlutLab.io <>
2022-03-31 09:30:15 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,53 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: BelajarListView(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({required this.title});
-
+class BelajarListView extends StatelessWidget {
+  final List bulan = [
+    "Januari",
+    "Fabruari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text("belajar layout"),
       ),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(15),
-            child: Text('Selamat datang pada pembelajaran flutter', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          ),
-          Container(
-            padding: EdgeInsets.all(15),
-            child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', style: TextStyle(fontSize: 16)),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+                title: Text(bulan[index], style: TextStyle(fontSize: 30)),
+                subtitle: Text('ini subtitle dari ' + bulan[index]),
+                leading: CircleAvatar(
+                  child: Text(bulan[index][0], // ambil karakter pertama text
+                      style: TextStyle(fontSize: 20)),
+                )),
+          );
+        },
+        itemCount: bulan.length,
       ),
     );
   }
