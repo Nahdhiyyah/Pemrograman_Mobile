cfdb84b6e8ad84b27debae48ff7551bfecd5426c
FlutLab.io <>
2022-03-31 11:47:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class cobaListView extends StatelessWidget {
                 height: 200,
                 fit: BoxFit.cover,
               )),
-              subtitle: Text(sub[index], style: TextStyle(color: Colors.brown, fontWeight: FontWeight.w300, fontSize: 15)),
+              subtitle: Text(sub[index], style: TextStyle(color: Colors.brown, fontWeight: FontWeight.bold, fontSize: 15)),
             ),
           );
         },
