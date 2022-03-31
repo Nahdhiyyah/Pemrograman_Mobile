34852baedfca80346ed12d4604a8cc7901951493
FlutLab.io <>
2022-03-31 11:09:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class cobaListView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Galery", style: TextStyle(color: Colors.black12, fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.indigo[100],
+        backgroundColor: Colors.indigo[50],
       ),
       body: ListView.builder(
         itemCount: gambar.length,
