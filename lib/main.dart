9c718b2ca61c24e9e6c66a8e96218a3919aca9b3
FlutLab.io <>
2022-04-04 07:14:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,70 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: cobaListView(),
+      home: BelajarAppBar(),
     );
   }
 }
 
-class cobaListView extends StatelessWidget {
-  final List<String> gambar = [
-    "assets/images/1.jpg",
-    "assets/images/2.jpg",
-    "assets/images/3.jpg",
-    "assets/images/4.jpg",
-    "assets/images/5.jpg",
-    "assets/images/6.jpg",
-    "assets/images/7.jpg",
-    "assets/images/8.jpg",
-    "assets/images/9.jpg",
-    "assets/images/10.jpg",
-    "assets/images/11.jpg",
-  ];
-
-  final List<String> sub = [
-    'Gambar 1',
-    'Gambar 2',
-    'Gambar 3',
-    'Gambar 4',
-    'Gambar 5',
-    'Gambar 6',
-    'Gambar 7',
-    'Gambar 8',
-    'Gambar 9',
-    'Gambar 10',
-    'Gambar 11'
-  ];
-
-  @override
+class BelajarAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text("My Galery", style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold)),
-        ),
-        backgroundColor: Colors.red[200],
-      ),
-      body: ListView.builder(
-        itemCount: gambar.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: ClipRRect(
-                  child: Image.asset(
-                gambar[index],
-                width: 350,
-                height: 200,
-                fit: BoxFit.cover,
-              )),
-              subtitle: Text(sub[index], style: TextStyle(color: Colors.brown, fontWeight: FontWeight.bold, fontSize: 15)),
-            ),
-          );
-        },
-      ),
-    );
+        body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                    expandedHeight: 200.0,
+                    floating: false,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                        centerTitle: true,
+                        title: Text("Belajar Sliver App Bar",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            )),
+                        background: Image(
+                          image: AssetImage('assets/images/1.jpg'),
+                          fit: BoxFit.cover,
+                        )))
+              ];
+            },
+            body: Center(
+              child: Text("Gambar Kiyowo"),
+            )));
   }
 }
