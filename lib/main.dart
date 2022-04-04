a6a747d4c4fb9abdff516eda7a5fdc0a67a6787f
FlutLab.io <>
2022-04-04 15:13:15 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -63,9 +63,6 @@ class BelajarAppBar extends StatelessWidget {
               ),
             ];
           },
-          body: ListTile(
-            title: Text(lagu[1]),
-          ),
         ),
       ),
     );
