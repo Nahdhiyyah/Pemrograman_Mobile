fea04c042e7a8b452eedc01dccfc114f99f96f3a
FlutLab.io <>
2022-04-04 15:43:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'lagu.dart' as lagu;
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,32 @@ final List<String> lagu = [
   "Lagu 2",
   "Lagu 3"
 ];
+
+// class lagu {
+//   lagu() {
+//     final List<String> lagu = [
+//       "Lagu 1",
+//       "Lagu 2",
+//       "Lagu 3"
+//     ];
+//   }
+// }
+
+// class galeri {
+//   final List<String> video = [
+//     "Video 1",
+//     "Video 2",
+//     "Video 3"
+//   ];
+// }
+
+// class archive {
+//   final List<String> arsip = [
+//     "Lagu 1",
+//     "Lagu 2",
+//     "Lagu 3"
+//   ];
+// }
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,43 +54,45 @@ class BelajarAppBar extends StatelessWidget {
       body: DefaultTabController(
         length: 3,
         child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: true,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Nadia's List",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                      )),
-                  background: Image(
-                    image: AssetImage('assets/images/15.jpg'),
-                    fit: BoxFit.cover,
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: true,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text("Nadia's List",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        )),
+                    background: Image(
+                      image: AssetImage('assets/images/15.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  backgroundColor: Colors.black54,
+                ),
+                SliverPersistentHeader(
+                  pinned: true,
+                  delegate: _SliverAppBarDelegate(
+                    TabBar(
+                      labelColor: Colors.black87,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        new Tab(icon: new Icon(Icons.audiotrack), text: "Lagu"),
+                        new Tab(icon: new Icon(Icons.collections), text: "Galeri"),
+                        new Tab(icon: new Icon(Icons.archive), text: "Arsip"),
+                      ],
+                    ),
                   ),
                 ),
-                backgroundColor: Colors.black54,
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: _SliverAppBarDelegate(
-                  TabBar(
-                    labelColor: Colors.black87,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      new Tab(icon: new Icon(Icons.audiotrack), text: "Lagu"),
-                      new Tab(icon: new Icon(Icons.collections), text: "Galeri"),
-                      new Tab(icon: new Icon(Icons.archive), text: "Arsip"),
-                    ],
-                  ),
-                ),
-              ),
-            ];
-          },
-        ),
+              ];
+            },
+            body: TabBarView(children: <Widget>[
+              ListTile(title: Text("Nadia")),
+            ])),
       ),
     );
   }
