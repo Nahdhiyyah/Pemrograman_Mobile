3a10dafcb60186223a88e334c8ce67f5f0c68f89
FlutLab.io <>
2022-04-04 17:00:39 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
 
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return new Container(child: _tabBar, color: Colors.pink[100]);
+    return new Container(child: _tabBar, color: Colors.pink[50]);
   }
 
   @override
